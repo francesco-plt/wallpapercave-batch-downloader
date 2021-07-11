cca2578a151dbf3744f52c68fa64f09d7295910e
Francesco Pallotta <26849744+plt-francesco@users.noreply.github.com>
2021-07-11 13:10:32 +0200

    Initial commit

---
+++ wallpapercave-batch-downloader.py
@@ -0,0 +1,49 @@
+#!/usr/local/bin/python3
+import urllib3
+import requests
+from time import sleep
+import os
+from bs4 import BeautifulSoup
+import pathlib
+from progress.bar import Bar
+import subprocess
+
+pwd = pathlib.Path(__file__).parent.resolve()
+
+# specify the url
+url = input("wallpapercave collection url: ")
+
+# query the website and return the html to the variable ‘page’
+print("requesting '", url, "'...")
+http = urllib3.PoolManager()
+response = http.request('GET', url)
+
+# parse the html using beautiful soup and store in variable `soup`
+soup = BeautifulSoup(response.data, "html.parser")
+
+links = []
+for i in soup.find_all("a", {"class":"download"}):
+    links.append("https://wallpapercave.com" + i['href'])
+print("found ", len(links), " links")
+
+# creating download folder
+foldername = os.path.join(str(pwd), url.rsplit('/',1)[1])
+
+try:
+    pathlib.Path(foldername).mkdir(parents=True, exist_ok=True)
+except OSError:
+    print("Creation of the directory '", pdw, "' failed")
+else:
+    print("Successfully created/reused the directory '", foldername, "'")
+
+with Bar("downloading...") as bar:
+    for link in links:
+        tmp = requests.get(link)
+        filepath = os.path.join(str(pwd), foldername, link.rsplit('/',1)[1].replace("?", "") + ".png")
+        open(filepath, 'wb').write(tmp.content)
+        sleep(0.02)
+        bar.next()
+
+print("finished")
+print("opening download folder...")
+os.startfile(foldername)
